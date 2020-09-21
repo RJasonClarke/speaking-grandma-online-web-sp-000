1a69155ec34975a824bab6f887d3411843eba26c
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-21 23:54:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,12 +4,10 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.downcase
     return "HUH? SPEAK UP, SONNY!"
-  elsif phrase == phrase.downcase
-    return "HUH? SPEAK UP, SONNY!"
   elsif phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else
-    return "No, NOT SINCE 1938!"
+    return "HUH? SPEAK UP, SONNY!"
   end
 end
 
