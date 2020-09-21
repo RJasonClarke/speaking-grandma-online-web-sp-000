414d841edc0868cff91ea39878d66099686a0a49
Robert Clarke <r.jason.clarke@gmail.com>
2020-09-21 23:46:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,10 @@
 # Write a speak_to_grandma method.
+def speak_to_grandma
   if speak_to_grandma == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else "HUH? SPEAK UP, SONNY!"
   end
-
+end
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
